6e884b3f4654d20bbc5a07cb8aad242cd99a42ff
Nilesh <nileshk@andrew.cmu.edu>
2018-04-29 02:53:21 -0400

    merge

--- weakly-supvervized-temp/modular_attention/data/ucf101.py
+++ weakly-supvervized-temp/modular_attention/data/ucf101.py
@@ -11,140 +11,146 @@ import collections
 
 
 class UCF101(Dataset):
-    def __init__(self, dataset_name, opts):
-        self._ucf_dir = osp.join(opts.ucf_dir, "{}_features".format(dataset_name))
-        self._ignore_names = [".", ".."]
-        self._feature_size = opts.feature_size
-
-        self._file_names = []
-        self._labels = []
-        self.class_labels(dataset_name, opts.labels_dir)
-
-        self._labels = []
-        for file in os.listdir(self._ucf_dir):
-            if file not in self._ignore_names:
-                self._file_names.append(file)
-                video_index = self.video2index[file]
-                self._labels.append(self.video_labels[video_index])
-        self._labels = np.stack(self._labels)
-        self._num_classes = opts.num_classes
-        self._combine_startegy = opts.combine_strategy
-        self._segments = opts.segments
-        self._labels = torch.from_numpy(self._labels).float()
-        # self._labels = torch.Tensor(len(self._file_names), self._num_classes).float().zero_()
-
-    def __len__(self):
-        return len(self._file_names)
-
-    def __getitem__(self, item):
-        ## returns the feature vector for the video
-        flow_features, rgb_features, numInputs = self.forward_video(item)
-        label = self.forward_label(item)
-        data = dict()
-        data['flow'] = flow_features
-        data['rgb'] = rgb_features
-        data['label'] = label
-        data['numInputs'] = numInputs
-        return data
-
-    def class_labels(self, name, labels_dir):
-        ##
-
-        class2index_file = osp.join(labels_dir, 'class_dict.csv')
-        video2index_file = osp.join(labels_dir, 'video_indices_{}.csv'.format(name))
-        video2labels_file = osp.join(labels_dir, 'class_labels_{}.npy'.format(name))
-
-        self.class2index = dict()
-        self.video2index = dict()
-        self.video_labels = None
-        with open(class2index_file, 'r') as csvfile:
-            reader = csv.reader(csvfile)
-            for row in reader:
-                self.class2index[row[0]] = int(row[1])
-
-        with open(video2index_file, 'r') as csvfile:
-            reader = csv.reader(csvfile)
-            for row in reader:
-                self.video2index[row[0]] = int(row[1])
-
-        self.video_labels = np.load(video2labels_file)
-
-    def forward_label(self, index):
-        return Variable(self._labels[index]).cuda()
-
-    def forward_video(self, index):
-        filename = self._file_names[index]
-        flow_file = osp.join(self._ucf_dir, filename, 'features_flow.npy')
-        rgb_file = osp.join(self._ucf_dir, filename, 'features_rgb.npy')
-
-        flow_features = np.load(open(flow_file, 'r'))
-        rgb_features = np.load(open(rgb_file, 'r'))
-
-        flow_segments = np.zeros((self._segments, self._feature_size), dtype=np.float32)
-        rgb_segments = np.zeros((self._segments, self._feature_size),
-                                dtype=np.float32)
-
-        frames = flow_segments.shape[0]
-        segment_len = flow_features.shape[0] // self._segments + 1
-        total_segments = flow_features.shape[0]
-        numInputs = np.expand_dims(np.array([flow_features.shape[0]]), axis=0)
-
-        if self._combine_startegy == 'uniform':
-            for i in range(self._segments):
-                start = (i * segment_len)
-                end = (i + 1) * segment_len
-                seq = np.arange(start, end)
-                flow_segments[i, :] = np.mean(np.take(flow_features, seq, axis=0, mode='wrap'), axis=0)
-                rgb_segments[i, :] = np.mean(np.take(rgb_features, seq, axis=0, mode='wrap'), axis=0)
-
-        if self._combine_startegy == 'strat1':
-            offset = np.random.choice(segment_len, 1)
-            for i in range(self._segments):
-                start = (i * segment_len) + offset
-                end = (i + 1) * segment_len + offset
-                seq = np.arange(start, end)
-                flow_segments[i, :] = np.mean(
-                    np.take(flow_features, seq, axis=0, mode='wrap'),
-                    axis=0)
-                rgb_segments[i, :] = np.mean(
-                    np.take(rgb_features, seq, axis=0, mode='wrap'),
-                    axis=0)
-
-        if self._combine_startegy == 'strat2':
-            for i in range(self._segments):
-                start = (i * segment_len)
-                end = (i + 1) * segment_len
-                if segment_len > 1:
-                    seq = np.random.choice(segment_len, int(segment_len * 0.8)) + start
-                else:
-                    seq = np.arange(start, end)
-                flow_segments[i, :] = np.mean(
-                    np.take(flow_features, seq, axis=0, mode='wrap'),
-                    axis=0)
-                rgb_segments[i, :] = np.mean(
-                    np.take(rgb_features, seq, axis=0, mode='wrap'),
-                    axis=0)
-        if self._combine_startegy == 'strat3':
-            for i in range(self._segments):
-                sample_range = total_segments
-                while sample_range < self._segments + 1:
-                    sample_range = sample_range + total_segments
-
-                sampledN = np.round(np.linspace(0, sample_range, self._segments + 1)).astype(np.int32)
-                randoms = np.random(self._segments)
-
-                differences = sampledN[1:] - sampledN[0:-1]
-                K = sampledN[0:-1] + np.round(randoms * differences).astype(np.int)
-                flow_segments = flow_features[K, :]
-                rgb_segments = rgb_features[K, :]
-
-        ## rgb_feautes are of the T depending on the length of the video.
-        #  Each segment has 1024 dimensional feature.
-        flow_segments = Variable(torch.from_numpy(flow_segments).cuda())
-        rgb_segments = Variable(torch.from_numpy(rgb_segments).cuda())
-        numInputs = Variable(torch.from_numpy(numInputs))
-
-        return flow_segments, rgb_segments, numInputs
+  def __init__(self, dataset_name, opts):
+    self._ucf_dir = osp.join(opts.ucf_dir, "{}_features".format(dataset_name))
+    self._ignore_names = [".", ".."]
+    self._feature_size = opts.feature_size
+    
+    self._file_names = []
+    self._labels = []
+    self.class_labels(dataset_name, opts.labels_dir)
+    
+    self._labels = []
+    for file in os.listdir(self._ucf_dir):
+      if file not in self._ignore_names:
+        self._file_names.append(file)
+        video_index = self.video2index[file]
+        self._labels.append(self.video_labels[video_index])
+    self._labels = np.stack(self._labels)
+    self._num_classes = opts.num_classes
+    self._combine_startegy = opts.combine_strategy
+    self._segments = opts.segments
+    self._labels = torch.from_numpy(self._labels).float()
+    # self._labels = torch.Tensor(len(self._file_names), self._num_classes).float().zero_()
+  
+  def __len__(self):
+    return len(self._file_names)
+  
+  def __getitem__(self, item):
+    ## returns the feature vector for the video
+    flow_features, rgb_features, numInputs = self.forward_video(item)
+    label = self.forward_label(item)
+    data = dict()
+    data['flow'] = flow_features
+    data['rgb'] = rgb_features
+    data['label'] = label
+    data['numInputs'] = numInputs
+    return data
+  
+  def class_labels(self, name, labels_dir):
+    ##
+    
+    class2index_file = osp.join(labels_dir, 'class_dict.csv')
+    video2index_file = osp.join(labels_dir, 'video_indices_{}.csv'.format(name))
+    video2labels_file = osp.join(labels_dir, 'class_labels_{}.npy'.format(name))
+    
+    self.class2index = dict()
+    self.video2index = dict()
+    self.video_labels = None
+    with open(class2index_file, 'r') as csvfile:
+      reader = csv.reader(csvfile)
+      for row in reader:
+        self.class2index[row[0]] = int(row[1])
+    
+    with open(video2index_file, 'r') as csvfile:
+      reader = csv.reader(csvfile)
+      for row in reader:
+        self.video2index[row[0]] = int(row[1])
+    
+    self.video_labels = np.load(video2labels_file)
+  
+  def forward_label(self, index):
+    return Variable(self._labels[index]).cuda()
+  
+  def forward_video(self, index):
+    filename = self._file_names[index]
+    flow_file = osp.join(self._ucf_dir, filename, 'features_flow.npy')
+    rgb_file = osp.join(self._ucf_dir, filename, 'features_rgb.npy')
+    
+    flow_features = np.load(open(flow_file, 'r'))
+    rgb_features = np.load(open(rgb_file, 'r'))
+    
+    flow_segments = np.zeros((self._segments, self._feature_size),
+                             dtype=np.float32)
+    rgb_segments = np.zeros((self._segments, self._feature_size),
+                            dtype=np.float32)
+    
+    frames = flow_segments.shape[0]
+    segment_len = flow_features.shape[0] // self._segments + 1
+    total_segments = flow_features.shape[0]
+    numInputs = np.expand_dims(np.array([flow_features.shape[0]]), axis=0)
+    
+    if self._combine_startegy == 'uniform':
+      for i in range(self._segments):
+        start = (i * segment_len)
+        end = (i + 1) * segment_len
+        seq = np.arange(start, end)
+        flow_segments[i, :] = np.mean(
+          np.take(flow_features, seq, axis=0, mode='wrap'), axis=0)
+        rgb_segments[i, :] = np.mean(
+          np.take(rgb_features, seq, axis=0, mode='wrap'), axis=0)
+    
+    if self._combine_startegy == 'strat1':
+      offset = np.random.choice(segment_len, 1)
+      for i in range(self._segments):
+        start = (i * segment_len) + offset
+        end = (i + 1) * segment_len + offset
+        seq = np.arange(start, end)
+        flow_segments[i, :] = np.mean(
+          np.take(flow_features, seq, axis=0, mode='wrap'),
+          axis=0)
+        rgb_segments[i, :] = np.mean(
+          np.take(rgb_features, seq, axis=0, mode='wrap'),
+          axis=0)
+    
+    if self._combine_startegy == 'strat2':
+      for i in range(self._segments):
+        start = (i * segment_len)
+        end = (i + 1) * segment_len
+        if segment_len > 1:
+          seq = np.random.choice(segment_len, int(segment_len * 0.8)) + start
+        else:
+          seq = np.arange(start, end)
+        flow_segments[i, :] = np.mean(
+          np.take(flow_features, seq, axis=0, mode='wrap'),
+          axis=0)
+        rgb_segments[i, :] = np.mean(
+          np.take(rgb_features, seq, axis=0, mode='wrap'),
+          axis=0)
+    if self._combine_startegy == 'strat3':
+      for i in range(self._segments):
+        sample_range = total_segments
+        while sample_range < self._segments + 1:
+          sample_range = sample_range + total_segments
+        
+        sampledN = np.round(
+          np.linspace(0, sample_range, self._segments + 1)).astype(np.int32)
+        # import pdb;pdb.set_trace()
+        
+        differences = sampledN[1:] - sampledN[0:-1]
+        randoms = np.random.rand(self._segments)
+        K = sampledN[0:-1] + np.round(randoms * differences).astype(np.int)
+        K = np.mod(K, np.ones(K.shape) * total_segments).astype(np.int)
+        flow_segments = flow_features[K, :]
+        rgb_segments = rgb_features[K, :]
+    
+    ## rgb_feautes are of the T depending on the length of the video.
+    #  Each segment has 1024 dimensional feature.
+    flow_segments = Variable(torch.from_numpy(flow_segments).cuda())
+    rgb_segments = Variable(torch.from_numpy(rgb_segments).cuda())
+    numInputs = Variable(torch.from_numpy(numInputs))
+    
+    return flow_segments, rgb_segments, numInputs
 
 
 class UCF101Temporal(Dataset):
@@ -444,7 +450,7 @@ class UCF101_modular(Dataset):
 
 
 def split(data_dir):
-    all_files = []
-    for file in os.listdir(data_dir):
-        all_files.append(file)
-    return all_files
+  all_files = []
+  for file in os.listdir(data_dir):
+    all_files.append(file)
+  return all_files
